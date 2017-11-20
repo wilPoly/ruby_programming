4694b28fce9f522db5500066e5b05fd182fb0f14
wilPoly <misterwrinkle@gmail.com>
2017-11-20 23:52:44 +0100

    Started to implement hash, array and range handling. Needs testing and refactoring

--- basic_ruby/advanced_building_blocks/my_enumerable.rb
+++ basic_ruby/advanced_building_blocks/my_enumerable.rb
@@ -1,8 +1,24 @@
 module Enumerable
 
   def my_each
-    for i in 0...self.length do
-      yield self[i]
+     if block_given?
+      case self.class
+      when Hash
+        0.upto(self.length-1) do |k, v|
+          yield (k, self[k])
+        end
+      when Array || Range
+        0.upto(self.length-1) do |i|
+          yield self[i]
+        end
+      when Range
+        0.upto(self.to_a.length-1)
+
+      0.upto(self.length-1) do |i|
+        yield self[i]
+      end
+    else
+      self.to_enum :my_each
     end
   end
 
