7663a82c0d1b36d0cf1f8013a17e0210f5345124
wilPoly <misterwrinkle@gmail.com>
2017-11-26 23:12:19 +0100

    Done upto my_any?, all working, handling different data collections and lack of block given

--- basic_ruby/advanced_building_blocks/my_enumerable.rb
+++ basic_ruby/advanced_building_blocks/my_enumerable.rb
@@ -1,28 +1,74 @@
 module Enumerable
 
   def my_each
-     if block_given?
-      case self.class
-      when Hash
-        0.upto(self.length-1) do |k, v|
-          yield (k, self[k])
-        end
-      when Array || Range
-        0.upto(self.length-1) do |i|
-          yield self[i]
-        end
-      when Range
-        0.upto(self.to_a.length-1)
-
-      0.upto(self.length-1) do |i|
-        yield self[i]
+    if block_given?
+      for i in self
+        yield i
       end
     else
       self.to_enum :my_each
     end
   end
 
-end
+  def my_each_with_index
+    if block_given?
+      for i in 0...self.length
+        yield(self[i], i)
+      end
+    else
+      self.to_enum :my_each_with_index
+    end
+  end
 
-array = (0..5).to_a
-array.my_each {|i| puts i}
+  def my_select
+    if block_given?
+      if self.class == Array
+        result = []
+        self.my_each do |i|
+          result << i if yield i
+        end
+        result
+      elsif self.class == Hash
+        result = {}
+        self.my_each do |k, v|
+          if yield(k, v)
+            result[k] = v
+          end
+        end
+        result
+      end
+    else
+      self.to_enum :my_select
+    end
+  end
+  
+  def my_all?
+    result = true
+    if block_given?
+      if self.class == Hash
+        self.my_each {|k, v| result = false unless yield(k, v)}
+        result
+      elsif self.class == Array
+        self.my_each {|i| result = false unless yield i}
+        result
+      end
+    else
+      self.my_all? {|obj| obj}
+    end
+  end
+
+  def my_any?
+    result = false
+    if block_given?
+      if self.class == Hash
+        self.my_each {|k, v| result = true if yield(k, v)}
+        result
+      elsif self.class == Array
+        self.my_each {|i| result = true if yield i}
+        result
+      end
+    else
+      self.my_any? {|obj| obj}
+    end
+  end
+end
