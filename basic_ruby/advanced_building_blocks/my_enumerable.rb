11daf9c626958762954a032bdd08038c06e0afc1
wilPoly <misterwrinkle@gmail.com>
2017-11-28 21:51:33 +0100

    Done 'my_none?' and 'my_count', seems to work

--- basic_ruby/advanced_building_blocks/my_enumerable.rb
+++ basic_ruby/advanced_building_blocks/my_enumerable.rb
@@ -71,4 +71,37 @@ module Enumerable
       self.my_any? {|obj| obj}
     end
   end
+
+  def my_none?
+    result = true
+    if block_given?
+      if self.class == Hash
+        self.my_any? {|k, v| result = false if yield(k, v)}
+        result
+      elsif self.class == Array
+        self.my_any? {|i| result = false if yield i}
+        result
+      end
+    else
+      self.my_none? {|obj| obj}
+    end
+  end
+
+  def my_count(*args)
+    result = 0
+    case
+      when args.size > 1
+        raise ArgumentError
+      when args.size == 1
+        value = args.first
+        self.my_each {|i| result += 1 if i == value}
+        result
+      when block_given?
+        self.my_each {|i| result += 1 if yield i}
+        result
+      else
+        self.my_each {|i| result += 1}
+        result
+    end
+  end
 end
